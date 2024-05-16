2d377a29889e5e1e1f22e499c3c2d1efdda7ab6b
islena caldas <islenacaldas@MacBook-Air-de-islena.local>
2024-05-16 10:06:31 -0500

    se han realizado cambios a proyecto

--- index_dom.js
+++ index_dom.js
@@ -1,10 +1,12 @@
 
 import hamburgerMenu from "./menu_hamburguesa.js";
-import { digitalClock, alarm} from "./dom/reloj.js";
+import { digitalClock, alarma } from "./dom/reloj.js"
+
 const d= document;
 
-d.addEventListener("DOMContentLoaded",(e) =>{
-    hamburgerMenu(".panel-btn",".panel",".menu a");
-    digitalClock("#reloj", "#activar-reloj","#desactivar-reloj")
-    alarm("assets/alarm-car-or-home-62554.mp3","#activar-alarma", "#desactivar-alarma")
+d.addEventListener("DOMContentLoaded",e=>{
+    hamburgerMenu(".panel-btn",".panel", ".menu a")
+    digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj")
+    alarma("/assets/super_minion_3.mp3","#activar-alarma", "#desactivar-alarma")
 })
+
