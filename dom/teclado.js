44f3013873e6bfbf151eb13635288b84af0e132d
IslenaCaldas <islenacaldas@gmail.com>
2024-05-20 11:53:22 -0500

    creacion de reloj de cuenta regresiva

--- dom/teclado.js
+++ dom/teclado.js
@@ -4,36 +4,48 @@ y=0
 
 export function moveBall(e, ball, stage){
     const $ball= d.querySelector(ball),
-    $stage = d.querySelector(stage);
+    $stage = d.querySelector(stage),
+    limitsBall = $ball.getBoundingClientRect(),
+    limitsStage = $stage.getBoundingClientRect();
+console.log(limitsBall, limitsStage)
    
-    const move= () => {
-        $ball.style.transform = `translate(${x*10}px, ${y*10}px)`;
-    }
+        
+
 
     switch(e.keyCode){
         case 37:
-            e.preventDefault();
-            x--;
-            move();
+            
+            if(limitsBall.left >limitsStage.left) {
+                e.preventDefault();
+                x--;
+            }
             break;
         case 38:
-            e.preventDefault();
-            y--;
-            move();
+            
+            if(limitsBall.top >limitsStage.top){
+                e.preventDefault();
+                y--;
+            }
+
             break;
         case 39:
-            e.preventDefault();
-            x++;
-            move();
+            
+            if(limitsBall.right < limitsStage.right) {
+                e.preventDefault();
+                x++;
+            }
             break;
         case 40:
-            e.preventDefault();
-            y++;
-            move()
+            
+            if(limitsBall.bottom <limitsStage.bottom){
+                e.preventDefault();
+                y++;
+            } 
             break;
         default:
             break; 
     }
+    $ball.style.transform = `translate(${x*10}px, ${y*10}px)`;
    
 }
 
--- index_dom.js
+++ index_dom.js
@@ -2,13 +2,18 @@
 import hamburgerMenu from "./menu_hamburguesa.js";
 import { digitalClock, alarma } from "./dom/reloj.js"
 import { moveBall, shortcuts} from "./dom/teclado.js";
+import  countdown from "./dom/cuenta_regresiva.js"
 
 const d= document;
 
 d.addEventListener("DOMContentLoaded",e=>{
     hamburgerMenu(".panel-btn",".panel", ".menu a")
     digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj")
-    alarma("/assets/super_minion_3.mp3","#activar-alarma", "#desactivar-alarma")
+    alarma("/assets/super_minion_3.mp3","#activar-alarma", "#desactivar-alarma"),
+    countdown("countdown",
+    "Nov 01, 2024 11:00:00",
+     "feliz cumple preciosa 🎉 "
+    )
 })
 
 d.addEventListener("keydown", e=>{
