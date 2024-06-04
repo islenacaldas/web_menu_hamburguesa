18aa16e0e0bcd098f2f59380e990a006cdc222ab
IslenaCaldas <islenacaldas@gmail.com>
2024-06-04 09:20:12 -0500

    creacion de sorteo utilizando javascript

--- dom/sorteo.js
+++ dom/sorteo.js
@@ -1,12 +1,12 @@
 const d= document;
 
 export default function draw(btn, selector){
-  const getWinner =(selector)=>{
+  const getWinnerComment =(selector)=>{
       const $players = d.querySelectorAll(selector),
       random= Math.floor(Math.random()* $players.length),
       winner =$players[random];
 
-      console.log($players, random, winner)
+      //console.log($players, random, winner)
       return `El ganador es: ${winner.textContent}`;
   }
   d.addEventListener("click", e=>{
@@ -14,5 +14,26 @@ export default function draw(btn, selector){
           let resultado= getWinner(selector);
           alert(resultado);
       }
-  })
+  });
+  
+
+  /*para hacer un sorto en tiempo real se debe buscar el selector que nos esta dando el nombre 
+  depende para donde estemos buscando hacer el sorteo;
+  con comentarios de youtube:
+   asi se ve el codigo--> 
+   este se pega en la consola de youtubeconst getWinnerComment =() => {
+    const $players = Array.from(document.querySelectorAll('#sections #contents #comment #body'));
+    if ($players.length === 0) {
+        return 'No hay comentarios.';
+    }
+    const randomIndex = Math.floor(Math.random() * $players.length);
+    const winnerComment = $players[randomIndex];
+    const winnerName = winnerComment.querySelector('#author-text span').textContent.trim();
+    const winnerCommentText = winnerComment.querySelector('#content-text').textContent;
+    return winnerComment ? `El ganador es: ${winnerName}, comentario: ${winnerCommentText}` : 'No se pudo obtener el ganador.';
 }
+// luego se coloca el buscador del comentario con la siguiente etiqueta creada directamente por google llamando a la funcion: 
+
+getWinnerComment("ytb-comment-thread-render")
+
+por ultimo nos da el resultado con un comentario al azar, diciendo que es el o la ganadora*/
