2a62ac345fb52441642528c1093c58f32c60b56f
IslenaCaldas <islenacaldas@gmail.com>
2024-06-07 11:19:14 -0500

    creacion de archivo js para el carrusel de fotos

---
+++ dom/carrusel.js
@@ -0,0 +1,7 @@
+const d= document; 
+
+export default function slider(){
+    const $nextBtn = d.querySelector(".slider-btns .next"),
+    $prevBtn= d.querySelector(".sliderBtn .prev"),
+    $slides = d.querySelectorAll(".slider-slide")
+}
