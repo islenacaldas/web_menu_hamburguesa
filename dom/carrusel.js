a49d044bb1469c626215bd79bd951e1b74046a03
IslenaCaldas <islenacaldas@gmail.com>
2024-06-07 11:43:57 -0500

    cambios al carrusel

--- dom/carrusel.js
+++ dom/carrusel.js
@@ -1,7 +1,23 @@
 const d= document; 
 
 export default function slider(){
-    const $nextBtn = d.querySelector(".slider-btns .next"),
-    $prevBtn= d.querySelector(".sliderBtn .prev"),
+    const $nextBtn = d.querySelector(".slider-btns.next"),
+    $prevBtn= d.querySelector(".slider-btns.prev"),
     $slides = d.querySelectorAll(".slider-slide")
+
+    let i= 0;
+
+    d.addEventListener("click", e=>{
+        if(e.target === $prevBtn){
+            e.preventDefault();
+            $slides[i].classList.remove("active");
+            i--;
+
+        if(i<0){
+            i= $slides.length -1;
+        }
+        $slides[i].classList.add("active");
+
+        }
+    })
 }
--- index_dom.js
+++ index_dom.js
@@ -12,6 +12,7 @@ import webCam from "./dom/deteccion_webcam.js";
 import getGeolocation from "./dom/geolocalizacion.js";
 import searchFilters from "./dom/filtro_busquedas.js";
 import draw from "./dom/sorteo.js";
+import slider from "./dom/carrusel.js";
 
 const d= document;
 
@@ -49,6 +50,7 @@ d.addEventListener("DOMContentLoaded",e=>{
     getGeolocation("geolocation")
     searchFilters(".card-filter",".card")
     draw("#winner-btn",".player")
+    slider()
 })
 
 d.addEventListener("keydown", e=>{
