66fbc0a44332cb541cee017208cfbd3a705ea272
IslenaCaldas <islenacaldas@gmail.com>
2024-06-11 10:08:59 -0500

    creacion del sistema para videos inteligentes

--- index_dom.js
+++ index_dom.js
@@ -14,6 +14,7 @@ import searchFilters from "./dom/filtro_busquedas.js";
 import draw from "./dom/sorteo.js";
 import slider from "./dom/carrusel.js";
 import scrollSpy from "./dom/scroll_espia.js";
+import smartVideo from "./dom/video_inteligente.js";
 
 const d= document;
 
@@ -53,6 +54,7 @@ d.addEventListener("DOMContentLoaded",e=>{
     draw("#winner-btn",".player")
     slider();
     scrollSpy();
+    smartVideo();
 })
 
 d.addEventListener("keydown", e=>{
