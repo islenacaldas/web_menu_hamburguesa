1c44c2eb688d9bf0e860e07d1b968280faacb055
IslenaCaldas <islenacaldas@gmail.com>
2024-06-18 10:01:41 -0500

    culminacion de validaciones en formularios con llavascript

--- dom/narrador.js
+++ dom/narrador.js
@@ -7,7 +7,30 @@ export default function speechReader(){
     $speechBtn = d.getElementById("speech-btn"),
     speechMessage= new SpeechSynthesisUtterance();
 
-    console.log(speechMessage);
+    //console.log(speechMessage);
 
     let voices =[];
+
+
+    d.addEventListener("DOMContentLoaded", (e)=>{
+       // console.log(w.speechSynthesis.getVoices())
+        //console.log(w.speechSynthesis)
+
+        w.speechSynthesis.addEventListener("voiceschanged", e=>{
+            //console.log(e)
+            voices= w.speechSynthesis.getVoices();
+            //console.log(voices);
+
+            voices.forEach((voice)=>{
+                const $option = d.createElement("option")
+                $option.value= voice.name;
+                $option.textContent = `${voice.name} *** ${voice.lang}`
+
+
+                $speechSelect.appendChild($option)
+            })
+        })
+    })
+        d.addEventListener("change", e=>{})
+    d.addEventListener("click", e=>{})
 }
