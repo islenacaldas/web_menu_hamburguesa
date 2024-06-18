5b660a441855e93a1c1d59e082cc24996eb9c802
IslenaCaldas <islenacaldas@gmail.com>
2024-06-18 10:33:32 -0500

    problema resuelto

--- dom/narrador.js
+++ dom/narrador.js
@@ -1,47 +1,35 @@
-const d= document, 
-w=window
+const d = document, w = window;
 
-export default function speechReader(){
-    const $speechSelect = d.getElementById("speech-select"),
-    $speechTextArea = d.getElementById("speech-text"),
-    $speechBtn = d.getElementById("speech-btn"),
-    speechMessage= new SpeechSynthesisUtterance();
+export default function speechReader() {
+   const $speechSelect = d.getElementById("speech-select"),
+   $speechTextArea = d.querySelector("textarea[name='speech-text']"),
+   $speechBtn = d.getElementById("speech-btn"),
+   speechMessage = new SpeechSynthesisUtterance();
 
-    //console.log(speechMessage);
+   let voices = [];
 
-    let voices =[];
+   d.addEventListener("DOMContentLoaded", e => {
+      w.speechSynthesis.addEventListener("voiceschanged", e => {
+         voices = w.speechSynthesis.getVoices();
+         voices.forEach(voice => {
+            const $option = d.createElement("option");
+            $option.value = voice.name;
+            $option.textContent = `${voice.name} *** ${voice.lang}`
+            $speechSelect.appendChild($option);
+         });
+      });
+   });
 
+   d.addEventListener("change", e => {
+      if(e.target === $speechSelect){
+         speechMessage.voice = voices.find(voice => voice.name === e.target.value);
+      }
+   });
 
-    d.addEventListener("DOMContentLoaded", (e)=>{
-       // console.log(w.speechSynthesis.getVoices())
-        //console.log(w.speechSynthesis)
-
-        w.speechSynthesis.addEventListener("voiceschanged", e=>{
-            //console.log(e)
-            voices= w.speechSynthesis.getVoices();
-            //console.log(voices);
-
-            voices.forEach((voice)=>{
-                const $option = d.createElement("option")
-                $option.value= voice.name;
-                $option.textContent = `${voice.name} *** ${voice.lang}`
-
-
-                $speechSelect.appendChild($option)
-            })
-        })
-    })
-        d.addEventListener("change", e=>{
-            if(e.target === $speechSelect){
-                speechMessage.voice = voices.find(
-                    voice=> voice.name === e.target.value); 
-            }
-            //console.log(speechMessage)
-        })
-    d.addEventListener("click", e=>{
-        if(e.target === $speechBtn){
-            speechMessage.text = $speechTextArea.value;
-            w.SpeechSynthesis.speak(speechMessage)
-        }
-    })
+   d.addEventListener("click", e => {
+      if(e.target === $speechBtn){
+         speechMessage.text = $speechTextArea.value;
+         w.speechSynthesis.speak(speechMessage);
+      }
+   })
 }
