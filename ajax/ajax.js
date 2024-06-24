8586f3b1cdad02a980bdcde9f5848745090168df
IslenaCaldas <islenacaldas@gmail.com>
2024-06-24 11:11:42 -0500

    ajax: libreria Axios

--- ajax/ajax.js
+++ ajax/ajax.js
@@ -98,3 +98,33 @@
 
     getData()
 })()
+
+
+(()=>{
+ const $axios= document.getElementsById("axios"),
+ $fragment = document.createDocumentFragment();
+
+ axios
+ .get("https://jsonplaceholder.typicode.com/users")
+ .then((res)=>{
+    console.log(res)
+    let json= res.data;
+
+    json.forEach((el)=>{
+        const $li= document.createElement("li");
+        $li.innerHTML = `${el.name}--${el.email}--${el.phone}`;
+        $fragment.appendChild($li);
+    })
+
+ $axios.appendChild($fragment);
+
+ })
+ .catch(err=>{
+    console.log(err.response);
+    let message = err.response.statusText || "ocurrio un error";
+    $axios.innerHTML = `Error ${err.response.status}: ${message}`
+ })
+ .finally(()=>{
+    console.log("Esto se ejecutara independientemente del resultado de Axios")
+ });
+})()
