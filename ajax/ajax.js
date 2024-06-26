4c879c17f0a7fe3b930207c86eafdfe15b52bdd6
IslenaCaldas <islenacaldas@gmail.com>
2024-06-26 15:14:02 -0500

    actualizacion

--- ajax/ajax.js
+++ ajax/ajax.js
@@ -131,5 +131,33 @@
 
 
 (()=>{
+    const $axiosAsync = document.getElementById("axios-async"),
+    $fragment = document.createDocumentFragment();
 
+    axios.get("https://jsonplaceholder.typicode.com/users");
+
+
+    async function getData(){
+        try{
+            let res= await axios.get("https://jsonplaceholder.typicode.com/users"),
+            json= await res.data;
+            //console.log(res, json) 
+
+        json.forEach(el=>{
+            const $li=  document.createElement("li");
+            $li.innerHTML = `${el.name}--${el.email}--${el.phone}`
+            $fragment.appendChild($li)
+            })
+
+            $axiosAsync.appendChild($fragment)
+        }catch(err){
+            //console.log(err.response)
+           let message = err.response.statusText||"ocurrio un error"
+            $axiosAsync.innerHTML= `Error ${err.response.status}: ${message}`
+        
+        }finally{
+            console.log(`esto se ejecutara independientemente del try catch`)
+        }
+    }
+    getData()
 })()
